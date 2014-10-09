94ccf7f0bea578fe8da782bcd40957d39689e099
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-09 06:59:15 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@92 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bms.c
+++ bms.c
@@ -274,7 +274,7 @@ void *thread_bms_read_service(void *arg) ___THREAD_ENTRY___
         memset(&frame, 0, sizeof(frame));
         nbytes = read(s, &frame, sizeof(struct can_frame));
         if ( (frame.can_id & 0xFFFF) != CAN_RCV_ID_MASK ) {
-            log_printf(DBG_LV1, "BMS: id not accept %x", frame.can_id);
+            log_printf(DBG_LV0, "BMS: id not accept %x", frame.can_id);
             continue;
         }
         if ( nbytes != sizeof(struct can_frame) ) {
