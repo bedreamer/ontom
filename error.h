3852b1c9a85a255444268fc0febdcf7aa871aec5
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-10 05:14:51 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@109 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- error.h
+++ error.h
@@ -12,6 +12,6 @@
 // 文件读写失败
 #define ERR_FILE_IO_FAILE (ERR_ERR|0x00000003)
 // 达到定时器最大个数
-#define ERR_TIMER_BEMAX  (ERR_ER|0x0000000004)
+#define ERR_TIMER_BEMAX  (ERR_ERR|0x0000000004)
 
 #endif /*_ERROR_H_*/
