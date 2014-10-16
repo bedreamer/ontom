391f18048505cbf0c9ad5e96d724df6ad01919bf
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-16 02:55:07 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@184 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bms.c
+++ bms.c
@@ -419,18 +419,18 @@ int about_packet_reciev_done(struct charge_task *thiz,
     case PGN_BCL :// 0x001000, BMS 电池充电需求报文
         memcpy(&thiz->bms_charge_need_now,
                param->buff.rx_buff, sizeof(struct pgn4096_BCL));
-        if ( thiz->bms_charge_need_now.spn3072_need_voltage > 750 ) {
+        if ( thiz->bms_charge_need_now.spn3072_need_voltage/10.0f > 750 ) {
             log_printf(WRN, "spn3072 range 0-750V gave: %d V",
                        thiz->bms_charge_need_now.spn3072_need_voltage);
         }
-        if ( thiz->bms_charge_need_now.spn3073_need_current > 400 ) {
+        if ( thiz->bms_charge_need_now.spn3073_need_current/10.0f > 400 ) {
             log_printf(WRN, "spn3073 range -400-0A gave: %d A",
                        thiz->bms_charge_need_now.spn3073_need_current);
         }
 
-        log_printf(INF, "PGN_BCL fetched, V-need: %d V, I-need: %d mode: %s",
-                   thiz->bms_charge_need_now.spn3072_need_voltage,
-                   thiz->bms_charge_need_now.spn3073_need_current,
+        log_printf(INF, "PGN_BCL fetched, V-need: %.1f V, I-need: %.1f mode: %s",
+                   thiz->bms_charge_need_now.spn3072_need_voltage/10.0,
+                   thiz->bms_charge_need_now.spn3073_need_current/10.0f,
                    thiz->bms_charge_need_now.spn3074_charge_mode == 0x01 ?
                        "恒压充电" :
                    thiz->bms_charge_need_now.spn3074_charge_mode == 0x02 ?
