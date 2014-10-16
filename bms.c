0f578d72cd79da1c29e3f53b87a41f7fa500d923
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-16 03:10:19 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@187 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bms.c
+++ bms.c
@@ -428,9 +428,9 @@ int about_packet_reciev_done(struct charge_task *thiz,
                        thiz->bms_charge_need_now.spn3073_need_current);
         }
 
-        log_printf(INF, "PGN_BCL fetched, V-need: %.1f V, I-need: %.1f mode: %s",
+        log_printf(INF, "PGN_BCL fetched, V-need: %.1f V, I-need: %d mode: %s",
                    thiz->bms_charge_need_now.spn3072_need_voltage/10.0,
-                   thiz->bms_charge_need_now.spn3073_need_current/10.0f,
+                   thiz->bms_charge_need_now.spn3073_need_current,
                    thiz->bms_charge_need_now.spn3074_charge_mode ==
                     CHARGE_WITH_CONST_VOLTAGE ? "恒压充电" :
                    thiz->bms_charge_need_now.spn3074_charge_mode ==
