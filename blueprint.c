fd7581d1e2222c9c650a2acf391ec943d33777b3
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-20 08:59:56 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@312 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -379,11 +379,11 @@ void *thread_uart_service(void *arg) ___THREAD_ENTRY___
             thiz->bp_evt_handle(thiz, BP_EVT_SWITCH_2_TX, NULL);
             retval = write(thiz->dev_handle, "01234560012345601234567712345677", 32);
             if ( retval ) {
-                log_printf(DBG_LV0, "WR:%d <%s>", retval, "0123456789");
+                log_printf(DBG_LV1, "WR:%d <%s>", retval, "0123456789");
                 //thiz->bp_evt_handle(thiz, BP_EVT_SWITCH_2_RX, NULL);
                 //thiz->status = BP_UART_STAT_RD;
             } else {
-                log_printf(DBG_LV0, "WR faile, %d-%d", retval, errno);
+                log_printf(DBG_LV1, "WR faile, %d-%d", retval, errno);
             }
             sleep(1);
             continue;
