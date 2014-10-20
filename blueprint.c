dde8206859f8b824c2cb7833ecc7c03f88d68d0b
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-20 02:42:03 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@217 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -367,7 +367,7 @@ void *thread_uart_service(void *arg) ___THREAD_ENTRY___
         }
 
         retval =
-            select(max_handle, &rd_set, &wr_set, NULL, &tv);
+            select(max_handle, &rd_set, NULL, NULL, &tv);
         if ( retval == -1 ) {
             log_printf(DBG_LV0, "toto..");
             continue;
