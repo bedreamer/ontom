cf9ebfcd4dfa096caaa3cf0c89fe4882e37e3ad2
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-20 08:12:29 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@301 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -29,6 +29,34 @@ struct bp_uart uarts[] = {
 #define	RX_LOW_LEVEL			0
 #define	TX_HIGH_LEVEL			1
 
+int set_speed(int fd, int speed)
+{
+    int   i;
+    int   status;
+    struct termios   Opt;
+
+    tcgetattr(fd, &Opt);
+
+    for ( i= 0;  i < sizeof(speed_arr) / sizeof(int);  i++)
+    {
+        if (speed == name_arr[i])
+        {
+            tcflush(fd, TCIOFLUSH);
+            cfsetispeed(&Opt, speed_arr[i]);
+            cfsetospeed(&Opt, speed_arr[i]);
+            status = tcsetattr(fd, TCSANOW, &Opt);
+            if  (status != 0)
+            {
+                perror("tcsetattr fd");
+                return -1;
+            }
+            tcflush(fd,TCIOFLUSH);
+            return 0;
+        }
+    }
+    return -1;
+}
+
 int configure_uart(int fd, int baud_rate, int databits, int stopbits, int parity)
 {
     struct termios options;
@@ -131,11 +159,10 @@ Return:
 void gpio_export(int pin)
 {
     // 导出所需要设置的GPIO口
-    fp = fopen("/sys/class/gpio/export","w");
+    FILE *fp = fopen("/sys/class/gpio/export","w");
     rewind(fp);
     fprintf(fp, "%d\n", pin);
     fclose(fp);
-
     return;
 }
 
@@ -148,11 +175,10 @@ Return:
 void gpio_unexport(int pin)
 {
     // 释放导出的GPIO口
-    fp = fopen("/sys/class/gpio/unexport","w");
+    FILE *fp = fopen("/sys/class/gpio/unexport","w");
     rewind(fp);
     fprintf(fp, "%d\n", pin);
     fclose(fp);
-
     return;
 }
 
