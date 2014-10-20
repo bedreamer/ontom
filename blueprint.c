ba7bc60ffd95904cbdf0ee3fbea0beb866194526
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-20 05:33:47 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@252 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -398,7 +398,7 @@ void *thread_uart_service(void *arg) ___THREAD_ENTRY___
 
             retval = FD_ISSET(thiz->dev_handle, &wr_set);
             thiz->bp_evt_handle(thiz, BP_EVT_SWITCH_2_TX, NULL);
-            usleep(100);
+            sleep(1);
             if ( retval ) {
                 retval = write(thiz->dev_handle, "0123456789", 11);
                 log_printf(DBG_LV1, "write out %d. ", retval);
