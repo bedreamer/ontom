50cebe7e8f409bb26cd3c93c8276c1e81bcdf468
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-21 08:02:47 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@335 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- log.c
+++ log.c
@@ -9,23 +9,55 @@ int log_printf(unsigned int level, const char *fmt, ...)
 	va_list ap;
 	char *lvlstr[] = {"INF", "WRN", "ERR", "DBG"};
     const char *cfg;
+    time_t timep;
+    struct tm *p;
 
+    time(&timep);
+    p =localtime(&timep);
 	va_start(ap, fmt);
 	vsnprintf(buff, sizeof(buff), fmt, ap);
 	va_end(ap);
 
     switch (level) {
     case INF:
-        printf("\033[32m[INF]\033[0m %s\n", buff);
+        printf("\033[32m[INF %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case WRN:
-        printf("\033[33m[WRN]\033[0m %s\n", buff);
+        printf("\033[33m[WRN %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case ERR:
-        printf("\033[31m[ERR]\033[0m %s\n", buff);
+        printf("\033[31m[ERR %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case DBG:
-        printf("\033[34m[DBG]\033[0m %s\n", buff);
+        printf("\033[34m[DBG %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+                buff);
         break;
     case DBG_LV0:
         cfg = config_read("debug_lvA");
@@ -34,7 +66,14 @@ int log_printf(unsigned int level, const char *fmt, ...)
              strcmp(cfg, "FALSE") == 0 ) {
             return;
         }
-        printf("\033[35m[DBG_LV0]\033[0m %s\n", buff);
+        printf("\033[35m[DBG_LV0 %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case DBG_LV1:
         cfg = config_read("debug_lvB");
@@ -43,7 +82,14 @@ int log_printf(unsigned int level, const char *fmt, ...)
              strcmp(cfg, "FALSE") == 0 ) {
             return;
         }
-        printf("\033[35m[DBG_LV1]\033[0m %s\n", buff);
+        printf("\033[35m[DBG_LV1 %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case DBG_LV2:
         cfg = config_read("debug_lvC");
@@ -52,7 +98,14 @@ int log_printf(unsigned int level, const char *fmt, ...)
              strcmp(cfg, "FALSE") == 0 ) {
             return;
         }
-        printf("\033[36m[DBG_LV2]\033[0m %s\n", buff);
+        printf("\033[36m[DBG_LV2 %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     case DBG_LV3:
         cfg = config_read("debug_lvD");
@@ -61,7 +114,14 @@ int log_printf(unsigned int level, const char *fmt, ...)
              strcmp(cfg, "FALSE") == 0 ) {
             return;
         }
-        printf("\033[36m[DBG_LV3]\033[0m %s\n", buff);
+        printf("\033[36m[DBG_LV3 %04d-%02d-%02d %02d:%02d:%02d]\033[0m %s\n",
+               p->tm_year,
+               p->tm_mon,
+               p->tm_mday,
+               p->tm_hour,
+               p->tm_min,
+               p->tm_sec,
+               buff);
         break;
     default:
         //return printf("%s\n", buff);
