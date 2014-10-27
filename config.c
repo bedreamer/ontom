37ff388725da21fd8bb9ecfd86e5c4bb22a06b0d
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-10-27 01:15:38 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@473 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- config.c
+++ config.c
@@ -653,7 +653,7 @@ int ajax_debug_list(struct ajax_xml_struct *thiz)
     for ( ; head && head->config_name != NULL && head->config_name[0]; head ++, nr ++ ) {
         log_printf(DBG_LV1, "get configure list from WEB. %d:%p:%d", nr, head, output_len);
         output_len += sprintf(&thiz->iobuff[output_len], "<tr><td id=\"t%d\">%s</td>",
-                              nr
+                              nr,
                               head->config_name);
         output_len += sprintf(&thiz->iobuff[output_len], "<td>%08X</td>", head->config_type);
         output_len += sprintf(&thiz->iobuff[output_len],
