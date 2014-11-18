995c01576550758453bce72705f603ac34a65733
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-11-18 02:26:57 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@594 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -22,8 +22,8 @@ static int uart4_bp_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
 struct bp_uart uarts[2];
 
 #define GPIO_TO_PIN(bank, gpio)	(32 * (bank) + (gpio))
-#define	SERIAL4_CTRL_PIN	GPIO_TO_PIN(0, 20)
-#define	SERIAL5_CTRL_PIN	GPIO_TO_PIN(0, 19)
+#define	SERIAL4_CTRL_PIN	GPIO_TO_PIN(0, 19)
+#define	SERIAL5_CTRL_PIN	GPIO_TO_PIN(0, 20)
 
 #define	RX_LOW_LEVEL			0
 #define	TX_HIGH_LEVEL			1
@@ -369,11 +369,11 @@ void *thread_uart_service(void *arg) ___THREAD_ENTRY___
 
     uarts[0].bp_evt_handle = uart4_bp_evt_handle;
     uarts[0].dev_handle = -1;
-    uarts[0].dev_name = "/dev/ttyO5";
+    uarts[0].dev_name = "/dev/ttyO4";
 
     uarts[1].bp_evt_handle = NULL;
     uarts[1].dev_handle = -1;
-    uarts[1].dev_name = "/dev/ttyO4";
+    uarts[1].dev_name = "/dev/ttyO5";
 
     if ( thiz ) {
         // 出错误后尝试的次数
