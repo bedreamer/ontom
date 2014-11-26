ee31b43f4f254ee68a0d85a91e12efadf46afbe6
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-11-26 02:22:46 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@620 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bms.c
+++ bms.c
@@ -493,6 +493,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
     case PGN_CEM :// 0x001F00
         break;
     case PGN_BRM :// 0x000200, BMS 车辆辨识报文
+        statistics[I_BRM].can_counter ++;
+
         if ( param->buff_payload == 8 ) {
             memcpy(&thiz->vehicle_info, param->buff.rx_buff, 8);
         } else if ( param->buff_payload == sizeof(struct pgn512_BRM) ) {
@@ -542,6 +544,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
         bit_set(thiz, F_BMS_RECOGNIZED);
         break;
     case PGN_BCP :// 0x000600, BMS 配置报文
+        statistics[I_BCP].can_counter ++;
+
         if ( param->buff_payload != 13 ) {
             log_printf(WRN, "BMS: BCP packet size crash, need 13 gave %d",
                        param->buff_payload);
@@ -595,6 +599,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
                    thiz->bms_config_info.spn2822_total_voltage);
         break;
     case PGN_BRO :// 0x000900, BMS 充电准备就绪报文
+        statistics[I_BRO].can_counter ++;
+
         log_printf(INF, "BMS is %s for charge.",
                    param->buff.rx_buff[0] == 0x00 ?
                     "not ready" :
@@ -611,6 +617,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
         }
         break;
     case PGN_BCL :// 0x001000, BMS 电池充电需求报文
+        statistics[I_BCL].can_counter ++;
+
         memcpy(&thiz->bms_charge_need_now,
                param->buff.rx_buff, sizeof(struct pgn4096_BCL));
         if ( thiz->bms_charge_need_now.spn3072_need_voltage/10.0f > 750 ) {
@@ -631,6 +639,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
                         CHARGE_WITH_CONST_CURRENT ? "恒流充电" : "无效模式");
         break;
     case PGN_BCS :// 0x001100, BMS 电池充电总状态报文
+        statistics[I_BCS].can_counter ++;
+
         log_printf(INF, "PGN_BCS fetched.");
         memcpy(&thiz->bms_all_battery_status, param->buff.rx_buff,
                sizeof(struct pgn4352_BCS));
@@ -652,6 +662,8 @@ int about_packet_reciev_done(struct charge_task *thiz,
         }
         break;
     case PGN_BSM :// 0x001300, 动力蓄电池状态信息报文
+        statistics[I_BSM].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BSM fetched.");
         memcpy(&thiz->bms_battery_status, param->buff.rx_buff,
                sizeof(struct pgn4864_BSM));
@@ -709,21 +721,33 @@ int about_packet_reciev_done(struct charge_task *thiz,
         }
         break;
     case PGN_BMV :// 0x001500, 单体动力蓄电池电压报文
+        statistics[I_BMV].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BMV fetched.");
         break;
     case PGN_BMT :// 0x001600, 单体动力蓄电池温度报文
+        statistics[I_BMT].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BMT fetched.");
         break;
     case PGN_BSP :// 0x001700, 动力蓄电池预留报文
+        statistics[I_BSP].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BSP fetched.");
         break;
     case PGN_BST :// 0x001900, BMS 中止充电报文
+        statistics[I_BST].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BST fetched.");
         break;
     case PGN_BSD :// 0x001C00, BMS 统计数据报文
+        statistics[I_BSD].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BSD fetched.");
         break;
     case PGN_BEM :// 0x001E00, BMS 错误报文
+        statistics[I_BEM].can_counter ++;
+
         log_printf(INF, "BMS: PGN_BEM fetched.");
         break;
     default:
@@ -1208,6 +1232,9 @@ int gen_packet_PGN1792(struct charge_task * thiz, struct event_struct* param)
     param->can_id =  gen->prioriy << 26 | gen->pgn << 8 | CAN_TX_ID_MASK | CAN_EFF_FLAG;
 
     param->evt_param = EVT_RET_OK;
+
+    statistics[I_CTS].can_counter ++;
+
     return 0;
 }
 
@@ -1227,6 +1254,9 @@ int gen_packet_PGN2048(struct charge_task * thiz, struct event_struct* param)
     param->can_id =  gen->prioriy << 26 | gen->pgn << 8 | CAN_TX_ID_MASK | CAN_EFF_FLAG;
 
     param->evt_param = EVT_RET_OK;
+
+    statistics[I_CML].can_counter ++;
+
     return 0;
 }
 
@@ -1245,6 +1275,9 @@ int gen_packet_PGN2560(struct charge_task * thiz, struct event_struct* param)
     param->can_id =  gen->prioriy << 26 | gen->pgn << 8 | CAN_TX_ID_MASK | CAN_EFF_FLAG;
 
     param->evt_param = EVT_RET_OK;
+
+    statistics[I_CRO].can_counter ++;
+
     return 0;
 }
 
@@ -1265,6 +1298,9 @@ int gen_packet_PGN4608(struct charge_task * thiz, struct event_struct* param)
     param->can_id =  gen->prioriy << 26 | gen->pgn << 8 | CAN_TX_ID_MASK | CAN_EFF_FLAG;
 
     param->evt_param = EVT_RET_OK;
+
+    statistics[I_CCS].can_counter ++;
+
     return 0;
 }
 
