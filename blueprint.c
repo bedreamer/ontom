788d825d416f03c90e8449328d49a4544cdc0df8
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-09 08:30:49 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@702 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -34,9 +34,9 @@ static int uart5_background_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
 struct bp_uart uarts[2];
 // 串口4 使用者为充电机和采样盒
 struct bp_user down_user[] = {
+    {120, 0, 5, 0, uart4_charger_config_evt_handle}, // 充电机参数寄存器(参数控制)，读写
     {101, 0, 5, 0, NULL/*uart4_simple_box_evt_handle*/},     // 采样
     {110, 0, 5, 0, NULL/*uart4_charger_evt_handle*/},        // 盒充电机运行寄存器，只读
-    {120, 0, 5, 0, uart4_charger_config_evt_handle}, // 充电机参数寄存器(参数控制)，读写
     {130, 0, 5, 0, NULL/*uart4_charger_module_evt_handle*/}, // 充电机参数寄存器(模块控制)，读写
     {140, 0, 5, 0, NULL/*uart4_charger_date_evt_handle*/},   // 充电机参数寄存器(日期时间)，读写
     {0,  0, 0, 0, NULL}
