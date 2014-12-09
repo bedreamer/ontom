d9ab547b2f420e95572883d8b69681795ded31f7
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-09 01:41:38 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@681 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -43,7 +43,7 @@ struct bp_user down_user[] = {
 };
 // 串口5 使用者为上位机
 struct bp_user up_user[] = {
-    {100, 0, 5, 0, NULL/*uart5_background_evt_handle*/},    // 采样盒
+    {100, 0, 5, 0, uart5_background_evt_handle},    // 采样盒
     {0,  0, 0, 0, NULL}
 };
 
@@ -344,6 +344,7 @@ static int uart4_bp_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
             return ERR_UART_CONFIG_FAILE;
         }
         self->status = BP_UART_STAT_WR;
+        self->hw_status = BP_UART_STAT_INVALID;
         break;
     // 关闭串口
     case BP_EVT_KILLED:
