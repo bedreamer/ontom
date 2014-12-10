1b2c5e6c0ad831827ffb2e89519add941edbb855
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-10 03:04:03 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@719 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -37,8 +37,8 @@ struct bp_user down_user[] = {
     {220, 0, 5, 0, uart4_charger_config_evt_handle}, // 充电机参数寄存器(参数控制)，读写
     {240, 0, 5, 0, uart4_charger_date_evt_handle},   // 充电机参数寄存器(日期时间)，读写
     {210, 0, 5, 0, uart4_charger_evt_handle},        // 盒充电机运行寄存器，只读
-    {230, 0, 5, 0, NULL/*uart4_charger_module_evt_handle*/}, // 充电机参数寄存器(模块控制)，读写
-    {201, 0, 5, 0, NULL/*uart4_simple_box_evt_handle*/},     // 采样
+    {230, 0, 5, 0, uart4_charger_module_evt_handle}, // 充电机参数寄存器(模块控制)，读写
+    {201, 0, 5, 0, uart4_simple_box_evt_handle},     // 采样
     {0,  0, 0, 0, NULL}
 };
 // 串口5 使用者为上位机
