8a6b688986ed66bb94ff82618054b7a6a537e7c3
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-11 07:35:48 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@759 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -845,9 +845,11 @@ static int uart4_simple_box_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
         if ( self->master->died < self->master->died_line ) {
             self->master->died ++;
         } else {
+            self->master->died ++;
             if ( ! bit_read(task, S_MEASURE_COMM_DOWN) ) {
             }
-            log_printf(ERR, "UART: "RED("综合采样盒通信中断, 请排查故障, 已禁止充电"));
+            log_printf(ERR, "UART: "RED("综合采样盒通信中断, 请排查故障,"
+                                        " 已禁止充电(%d)."), self->master->died);
             bit_set(task, S_MEASURE_COMM_DOWN);
         }
         log_printf(WRN, "UART: %s get signal TIMEOUT", __FUNCTION__);
--- blueprint.h
+++ blueprint.h
@@ -134,7 +134,7 @@ struct bp_user {
     // 允许连续丢帧的最大次数
     unsigned char died_line;
     // 连续丢失数据的次数
-    unsigned char died;
+    unsigned int died;
 
     // 使用者事件通知响应函数
     // 可接受的事件包括：
