b4a2bd1f38c32afa5f2ec09020d391e8b7133744
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-12 05:24:07 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@804 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -333,7 +333,7 @@ static int uart4_bp_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
         self->rx_param.buff_size = sizeof(self->rx_buff);
 
         self->users = &down_user[0];
-        self->master = &self->users[0];
+        self->master =NULL;// &self->users[0];
 
         ret = _Hachiko_new(&self->rx_seed, HACHIKO_AUTO_FEED,
                      1600, HACHIKO_PAUSE, (void*)self);
