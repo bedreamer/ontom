5ba6014e56763bfadc5774769713a762e9ba9279
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-15 01:59:37 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@824 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -1318,7 +1318,7 @@ int ajax_uart_debug_page(struct ajax_xml_struct *thiz)
          } else if (me->user_evt_handle == uart4_charger_evt_handle) {
             output_len += sprintf(&thiz->iobuff[output_len], "{\"obj\":\"监控遥信\",");
          } else {
-            log_printf(ERR, "AJAX: bad request abort UART debug page.");
+            log_printf(ERR, "AJAX: bad request abort UART debug page. %p", me->user_evt_handle);
             return ERR_ERR;
          }
         output_len += sprintf(&thiz->iobuff[output_len], "\"freq\":%d,", me->frame_freq);
