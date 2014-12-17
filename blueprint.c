fcefe5e05f23f472e51d8afea9fbb59f7dd1d967
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-17 07:57:01 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@922 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -986,7 +986,7 @@ static int uart4_simple_box_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
         qry.len = 16;
         qry.dc_output_hezha = 1;
         qry.gun_1_assit_power_on = 1;
-        qry.gun_1_output_hezha = 1;
+        //qry.gun_1_output_hezha = 1;
         qry.crc = load_crc(23, (char *)&qry);
         //qry.crc = l2b(qry.crc);
         memcpy(param->buff.tx_buff, &qry, sizeof(qry));
