36a8687c27f1e9f3ae0920ffd8b288be1f874f13
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-17 08:54:57 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@941 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -995,7 +995,8 @@ static int uart4_simple_box_evt_handle(struct bp_uart *self, BP_UART_EVENT evt,
 
         self->rx_param.need_bytes = 32;
         ret = ERR_OK;
-        log_printf(INF, "UART: %s sent %d %04X:%04X", __FUNCTION__, sizeof(qry),
+        log_printf(INF, "UART: %s sent %d %04X:%04X:%04X", __FUNCTION__, sizeof(qry),
+                   qry.crc,
                    load_crc(23, param->buff.tx_buff),
                    load_crc(23, (char *)&qry));
         break;
