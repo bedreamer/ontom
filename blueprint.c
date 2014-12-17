d17df71b1975ccd8ea4d01cb50417ff95357827a
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2014-12-17 02:10:16 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@889 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- blueprint.c
+++ blueprint.c
@@ -1393,7 +1393,7 @@ int ajax_uart_debug_page(struct ajax_xml_struct *thiz)
     }
 
     // 充电机遥信，遥测量
-    output_len += sprintf(&thiz->iobuff[output_len], ", chargers:{");
+    output_len += sprintf(&thiz->iobuff[output_len], "; chargers:{");
     output_len += sprintf(&thiz->iobuff[output_len], "\"charger_sn\":\"%08X\",", task->chargers.charger_sn);
     output_len += sprintf(&thiz->iobuff[output_len], "\"charger_status\":\"%08X\",", task->chargers.charger_status);
     output_len += sprintf(&thiz->iobuff[output_len], "\"charger_max_v_out\":%d,", task->chargers.charger_max_v_out);
