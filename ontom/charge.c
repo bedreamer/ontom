6d7ae7ffcc0320e908997670d5af38afa5d006d4
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-07 05:43:29 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1179 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -323,15 +323,15 @@ void deal_with_job_business(struct charge_task *thiz)
         }
 
         if ( ret == GUN_SN0 ) {
+            bit_set(task, CMD_GUN_1_ASSIT_PWN_ON);
             if ( ! bit_read(task, F_GUN_1_ASSIT_PWN_SWITCH_STATUS) ) {
                 if ( !bit_read(task, S_ASSIT_POWER_DOWN) ) {
-                    bit_set(task, CMD_GUN_1_ASSIT_PWN_ON);
                 }
             }
         } else {
+            bit_set(task, CMD_GUN_2_ASSIT_PWN_ON);
             if ( ! bit_read(task, F_GUN_2_ASSIT_PWN_SWITCH_STATUS) ) {
                 if ( !bit_read(task, S_ASSIT_POWER_DOWN) ) {
-                    bit_set(task, CMD_GUN_2_ASSIT_PWN_ON);
                 }
             }
 
