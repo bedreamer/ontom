02bffde534f14c08bb286f4196553e836f32aaad
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-07 06:12:45 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1189 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -311,7 +311,7 @@ void deal_with_job_business(struct charge_task *thiz)
     int ret = GUN_SN0;
     static int fl  = 0;
     if ( thiz->this_job == NULL ) return;
-    thiz->this_job->job_gun_sn = JOB_STANDBY;
+    thiz->this_job->job_status = JOB_STANDBY;
 
     switch ( thiz->this_job->job_status ) {
     case JOB_IDLE:
