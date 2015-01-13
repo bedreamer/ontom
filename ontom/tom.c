f29ca06551bc9e3655b9c0d9585133a430dbd7d6
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-13 06:05:50 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1252 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/tom.c
+++ ontom/tom.c
@@ -150,7 +150,7 @@ int main()
 
     signal(SIGINT, sig_interrupt);
 
-    ret = sqlite3_open(DEFAULT_DB, &database);
+    ret = sqlite3_open(DEFAULT_DB, &task->database);
     if ( ret == SQLITE_OK ) {
         log_printf(ERR, "TOM: 打开数据库失败..");
     }
