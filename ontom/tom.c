520a9c6cac4db204557f34d1909b3542de8d3e68
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-13 05:28:50 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1245 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/tom.c
+++ ontom/tom.c
@@ -17,7 +17,7 @@
 #include <linux/can/raw.h>
 #include "../thirdpart/mongoose/mongoose.h"
 #include "../thirdpart/D8/librf.h"
-#include "../thirdpart/sqlite/sqlite3ext.h"
+//#include "../thirdpart/sqlite/sqlite3ext.h"
 #include "../thirdpart/sqlite/sqlite3.h"
 #include "ajax.h"
 #include "config.h"
