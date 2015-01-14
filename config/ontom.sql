7cd91a0fd9b29e16891a40cf5d014aed6414230c
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-14 05:47:31 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1287 85aebdb3-f732-af43-9dbd-bcef24ea9265

---
+++ config/ontom.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE log(
+	timestamp string, 
+	logstring string
+	);
+CREATE TABLE errors(
+	error_id string,
+	error_begin string,
+	error_recover string,
+	recover_reason string,
+	error_string string
+	);
