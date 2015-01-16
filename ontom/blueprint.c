6d7e2841b90ca736c5b1b514d6077ad76d39a5f7
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-16 01:47:23 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1346 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -1832,7 +1832,8 @@ ___fast_switch_2_rx:
             thiz->tx_param.payload_size = 0;
             thiz->tx_param.cursor = 0;
 
-            if ( thiz->rx_seed.remain ) {
+            if ( thiz->rx_param.payload_size >=
+                 thiz->rx_param.need_bytes ) {
                 // 前一次接收没有超时，减缓节奏
                 log_printf(INF, "UART: 前一次通信正常.");
                 usleep(600 * 1000);
