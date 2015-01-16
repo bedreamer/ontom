365217c88d67d90b703dbb562ea86246f2d360de
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-16 06:19:43 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1383 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -90,8 +90,8 @@ int configure_uart(int fd, int baud_rate, int databits, int stopbits, int parity
         perror("SetupSerial 1");
         return ERR_UART_CONFIG_FAILE;
     }
-    cfsetispeed(&options, B9600);
-    cfsetospeed(&options, B9600);
+    cfsetispeed(&options, B19200);
+    cfsetospeed(&options, B19200);
     options.c_cflag |= (CLOCAL|CREAD);
 
     switch (databits) {
