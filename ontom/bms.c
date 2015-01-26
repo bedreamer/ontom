8d2af5ec238e2f5cfc3fb043d1dfddde57700a62
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-26 09:06:56 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1616 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/bms.c
+++ ontom/bms.c
@@ -14,7 +14,7 @@ void heart_beart_notify_proc(Hachiko_EVT evt, void* _private, const struct Hachi
         unsigned int i = 0;
         struct charge_job * thiz = (struct charge_job *)_private;
         struct can_pack_generator *gen, *me;
-        for ( i = 0; thiz->bms.can_pack_gen_nr; i++ ) {
+        for ( i = 0; i < thiz->bms.can_pack_gen_nr; i++ ) {
             debug_track();
             gen = & thiz->bms.generator[i];
             if ( task->this_job[0] && gen->stage == thiz->bms.charge_stage ) {
