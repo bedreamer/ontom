916273594b9966aca49179b07adeed848c5c0e33
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-01-26 08:17:48 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1598 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/tom.c
+++ ontom/tom.c
@@ -122,15 +122,27 @@ void sig_interrupt(int signo)
 
 void sig_dbg_interrupt(int n, struct siginfo *siginfo, void *myact)
 {
-#define SIZE 1000
-    void *buffer[SIZE];
-    int i, num;
-    char **calls;
-    printf("Fault address:%X\n",siginfo->si_addr);
-    num = backtrace(buffer, SIZE);
-    calls = backtrace_symbols(buffer, num);
-    for (i = 0; i < num; i++)
-            printf("%s\n", calls[i]);
+    int j, nptrs;
+    #define SIZE 100
+    void *buffer[100];
+    char **strings;
+
+    nptrs = backtrace(buffer, SIZE);
+    printf("backtrace() returned %d addresses\n", nptrs);
+
+    /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+      would produce similar output to the following: */
+
+    strings = backtrace_symbols(buffer, nptrs);
+    if (strings == NULL) {
+       perror("backtrace_symbols");
+       exit(EXIT_FAILURE);
+    }
+
+    for (j = 0; j < nptrs; j++)
+       printf("%s\n", strings[j]);
+
+    free(strings);
     exit(1);
 }
 
