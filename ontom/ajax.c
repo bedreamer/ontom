20ee98318ae06eb40cb46e51d4c9e111184b61d8
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-02-06 09:13:00 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1713 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/ajax.c
+++ ontom/ajax.c
@@ -943,7 +943,7 @@ int ajax_job_query_json_proc(struct ajax_xml_struct *thiz)
     struct charge_job *job;
     thiz->ct = "application/json";
     thiz->xml_len = 0;
-    thiz->xml_len += sprintf(&thiz->iobuff[thiz->xml_len], "[");
+    thiz->xml_len += sprintf(&thiz->iobuff[thiz->xml_len], "{[");
     for ( i = 0; i < CONFIG_SUPPORT_CHARGE_JOBS; i ++ ) {
         if ( task->job[i] == NULL ) continue;
         job_query_json_fromat(thiz, task->job[i]);
@@ -959,7 +959,7 @@ int ajax_job_query_json_proc(struct ajax_xml_struct *thiz)
         thiz->iobuff[--thiz->xml_len] = '\0';
         pthread_mutex_unlock (&task->wait_lck);
     }
-    thiz->xml_len += sprintf(&thiz->iobuff[thiz->xml_len], "]");
+    thiz->xml_len += sprintf(&thiz->iobuff[thiz->xml_len], "]}");
     return ret;
 }
 
