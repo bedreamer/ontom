b02e7ca16b7c03bf4d86449cb8ba4c1c40933840
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-02-09 10:24:45 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1776 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/ajax.c
+++ ontom/ajax.c
@@ -899,7 +899,7 @@ int ajax_job_create_json_proc(struct ajax_xml_struct *thiz)
     }
 
     //log_printf(INF, "jc.charge_mode == CHARGE_MANUAL %x", jc.charge_mode);
-    if ( jc.charge_mode == CHARGE_MANUAL ) {
+    if ( jc.charge_mode != CHARGE_AUTO ) {
         if ( strlen(set_V) <= 0 ) {
             log_printf(DBG_LV3, "1错误的充电参数 %s %x %x", c_mode, CHARGE_AUTO, CHARGE_MANUAL);
             goto reject;
