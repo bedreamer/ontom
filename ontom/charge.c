1d404b03f420944c938fbf6be867b86498081635
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-02-10 04:13:55 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1803 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -438,7 +438,7 @@ void *thread_charge_task_service(void *arg) ___THREAD_ENTRY___
             for( i = 0; i < task->sys_config_gun_nr; i ++ ) {
                 if ( task->job[ i ] == NULL ) continue;
                 job_running(task, task->job[ i ]);
-                if ( task->job[i]->job_status == JOB_DETACHING ) {
+                if ( task->job[i]->job_status == JOB_ABORTING ) {
                     free(task->job[i]);
                     log_printf(INF, "ZEUS: 作业中止");
                     task->job[i] = NULL;
@@ -1172,7 +1172,7 @@ void job_detach_wait(struct charge_task *tsk)
             thiz = list_load(struct charge_job, job_node, p);
             //log_printf(INF, "id: %08X %x", thiz->job_url_commit_timestamp,
             //           thiz->job_status);
-            if ( thiz->job_status != JOB_ABORTING ) {
+            if ( thiz->job_status != JOB_DETACHING ) {
                 p = p->next;
                 thiz = NULL;
                 continue;
