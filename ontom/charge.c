ae06aad38176601e237e7276a41a890f8cb8eacc
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-05 03:14:02 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1889 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -442,7 +442,7 @@ void *thread_charge_task_service(void *arg) ___THREAD_ENTRY___
     pthread_mutex_init(&task->wait_lck, NULL);
     pthread_mutex_init(&task->err_list_lck, NULL);
 
-    memset(task->single, 255, sizeof(task->single));
+    //memset(task->single, 255, sizeof(task->single));
 
     while ( 1 ) {
         // 清除无效的作业
