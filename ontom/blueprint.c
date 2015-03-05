c9c4ad5c4cbb7326a9e182d283fdb6d8c832e566
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-05 01:15:53 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1884 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -778,9 +778,11 @@ int uart4_charger_yaoce_50_100_handle(struct bp_uart *self, struct bp_user *me, 
         memcpy(&me->chargers->chargers.charge_module_status, &param->buff.rx_buff[3], 100);
         for (i = 0; i < CONFIG_SUPPORT_CHARGE_MODULE; i ++ ) {
             // 判断模块故障
-            param->buff.rx_buff[3] = param->buff.rx_buff[3] ^ param->buff.rx_buff[4];
-            param->buff.rx_buff[4] = param->buff.rx_buff[3] ^ param->buff.rx_buff[4];
-            param->buff.rx_buff[3] = param->buff.rx_buff[3] ^ param->buff.rx_buff[4];
+            if ( i % 2 == 0 ) {
+                param->buff.rx_buff[3 + i] = param->buff.rx_buff[3 + i] ^ param->buff.rx_buff[4 + i];
+                param->buff.rx_buff[4 + i] = param->buff.rx_buff[3 + i] ^ param->buff.rx_buff[4 + i];
+                param->buff.rx_buff[3 + i] = param->buff.rx_buff[3 + i] ^ param->buff.rx_buff[4 + i];
+            }
             if ( param->buff.rx_buff[i + 3] & 0x0F ) {
                 bit_set(task, S_CHARGE_M_1_ERR + i);
             } else {
