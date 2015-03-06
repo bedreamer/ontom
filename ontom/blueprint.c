957c3dcb60b7d8298f2b6dab83cbe29fa5bb4827
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-06 02:27:49 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@1921 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -1745,7 +1745,7 @@ int uart4_convert_box_write_evt_handle(struct bp_uart *self, struct bp_user *me,
         break;
     // 串口发送确认
     case BP_EVT_TX_FRAME_CONFIRM:
-        ret = ERR_OK;
+        //ret = ERR_OK;
         break;
     // 串口数据发送完成事件
     case BP_EVT_TX_FRAME_DONE:
