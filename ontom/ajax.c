db30ca6004ef53450bd377c49b672e68065c53af
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-13 05:31:59 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2041 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/ajax.c
+++ ontom/ajax.c
@@ -1406,11 +1406,15 @@ int ajax_system_config_save_proc(struct ajax_xml_struct *thiz)
             memset(value, 0, sizeof(value));
             keylen = 0;
             vallen = 0;
+            keyok = 0;
+            valok = 0;
         } else if ( valok && ! keyok ) {
             memset(key, 0, sizeof(key));
             memset(value, 0, sizeof(value));
             keylen = 0;
             vallen = 0;
+            keyok = 0;
+            valok = 0;
         }
         p++;
     }
