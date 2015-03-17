564ac44f96d924b28e1cf1137488889f4544abce
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-17 02:32:25 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2071 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/log.h
+++ ontom/log.h
@@ -13,18 +13,16 @@
 // 输出速度最慢
 #define DBG_LV3 0x03
 
-int _log_printf(size_t line, unsigned int level, const char *fmt, ...);
+int log_printf(unsigned int level, const char *fmt, ...);
 #define GRN(s) "\033[32m"s"\033[0m"
 #define RED(s) "\033[31m"s"\033[0m"
 #define YEL(s) "\033[33m"s"\033[0m"
 #define debug_log log_printf
 static inline __log_printf(int l, const char * f)
 {
-    _log_printf(__LINE__, DBG, "%s:%d tracked.", f, l);
+    log_printf(__LINE__, DBG, "%s:%d tracked.", f, l);
 }
 
 #define debug_track() __log_printf(__LINE__, __FILE__)
-
-#define log_printf(lv, fmt...) _log_printf(__LINE__, lv, fmt, __VA_ARGS__)
 
 #endif
