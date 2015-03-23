9418b516165281cf5239df0bc3c22cd8ba1ecbf0
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-23 09:47:30 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2205 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2392,7 +2392,7 @@ int card_reader_handle(struct bp_uart *self, struct bp_user *me, BP_UART_EVENT e
             } else {
                 struct user_card cd;
 
-                memcpy(cd.card.sector_4.buff, param->buff.rx_buff, 16);
+                memcpy(cd.card.sector_4.buff, &param->buff.rx_buff[4], 16);
                 if ( cd.card.sector_4.data.magic != 0x4F4E5057 ) {
                     log_printf(WRN, "UART: 无法识别的卡 %08X.", cd.card.sector_4.data.magic);
                 } else if ( cd.card.sector_4.data.sum !=
