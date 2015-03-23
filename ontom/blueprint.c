70c9e073e850d34b3efe58af693530e9534f8017
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-23 07:33:07 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2188 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2509,6 +2509,7 @@ int card_reader_handle(struct bp_uart *self, struct bp_user *me, BP_UART_EVENT e
     case BP_EVT_RX_FRAME_TIMEOUT:
         //self->master->died ++;
         log_printf(WRN, "UART: %s get signal TIMEOUT", __FUNCTION__);
+        query_stat = SEQ_FIND_CARD;
         break;
     // 串口IO错误
     case BP_EVT_IO_ERROR:
