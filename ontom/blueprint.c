1a82ca42a32fdc31b987bb9100c0243a809b96a4
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-24 02:06:42 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2242 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2173,7 +2173,7 @@ int kwh_meter_read_evt_handle(struct bp_uart *self, struct bp_user *me, BP_UART_
 
         memcpy(param->buff.tx_buff, buff, nr);
         param->payload_size = nr;
-        self->master->time_to_send = param->payload_size * 1000 / 218;
+        self->master->time_to_send = ( param->payload_size + 1) * 1000 / 218;
         self->rx_param.need_bytes = 36;
         log_printf(DBG_LV3, "UART: %s requested.", __FUNCTION__);
         ret = ERR_OK;
