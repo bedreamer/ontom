c196fa536bf662ddcd3f7e3929203b07c90c1612
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-26 06:19:29 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2311 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -3387,7 +3387,7 @@ continue_to_send:
                     int tts = 0;
                     tts = (int)(thiz->tx_param.payload_size *__usperbyte(thiz));
                     usleep(tts + thiz->master->swap_time_modify);
-                    log_printf(INF, "UART: packet send done. sleep: %d:%d us",
+                    log_printf(DBG_LV1, "UART: packet send done. sleep: %d:%d us",
                                tts, thiz->master->swap_time_modify);
                 } while (0);
 
