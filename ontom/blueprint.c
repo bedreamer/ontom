a3c461ccdb81c546b7aa42286179401c01d7368b
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-26 04:37:17 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2288 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2776,7 +2776,7 @@ int card_reader_handle(struct bp_uart *self, struct bp_user *me, BP_UART_EVENT e
 
             memcpy(param->buff.tx_buff, buff, nr);
             param->payload_size = nr;
-            self->master->time_to_send = param->payload_size * 1000 / 960;
+            self->master->time_to_send = (param->payload_size + 1) * 1000 / 960;
             self->rx_param.need_bytes = 7;
             log_printf(DBG_LV3, "UART: %s:SEQ_WRITE_PUBLIC_BLK requested.", __FUNCTION__);
             ret = ERR_OK;
