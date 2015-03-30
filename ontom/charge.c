45fcb4f82e5232a4713b4ea50952e7cc7a670502
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-30 01:33:46 +0000

    v
    git-svn-id: svn://192.168.1.8/ontom/trunk@2394 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -480,6 +480,7 @@ void *thread_charge_task_service(void *arg) ___THREAD_ENTRY___
     pthread_mutex_init(&task->wait_lck, NULL);
     pthread_mutex_init(&task->err_list_lck, NULL);
 
+    do {sleep(1);} while (1);
     //memset(task->single, 255, sizeof(task->single));
 
     while ( 1 ) {
