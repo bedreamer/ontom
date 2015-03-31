27e91183e1f0c4cc15baa4f5ee23ee5a5f2a29e7
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-03-31 09:33:01 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2444 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -6,12 +6,6 @@ echo "执行数据库预配置..."
 echo "开始启动主程序..."
 
 while [ true ]; do
-	/usr/zeus/zeus
-	echo "检测到程序主动退出, 1秒后自动重启..."
-	sleep 1;
-	echo "重新预配置数据库"
-	/usr/zeus/preconfig.sh /usr/zeus/ontom.db
-	echo "正在重启...."
 	kill -9 `pidof browser`
 	mode=`sqlite3 ontom.db "SELECT current_value FROM settings WHERE key='work_mode'"`
 	if [ mode -eq "normal" ]; then
@@ -23,4 +17,11 @@ while [ true ]; do
 	else
 		browser -qws "http://127.0.0.1:8080/" &
 	fi
+	/usr/zeus/zeus
+	echo "检测到程序主动退出, 1秒后自动重启..."
+	sleep 1;
+	echo "重新预配置数据库"
+	/usr/zeus/preconfig.sh /usr/zeus/ontom.db
+	echo "正在重启...."
+
 done
