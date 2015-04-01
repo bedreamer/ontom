72b63bfb8c083055abfbfdc2e145d436683aa97d
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-01 01:43:51 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2447 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -8,14 +8,18 @@ echo "开始启动主程序..."
 while [ true ]; do
 	kill -9 `pidof browser`
 	kill -9 `pidof zeus`
-	mode=`sqlite3 /usr/zeus/ontom.db "SELECT current_value FROM settings WHERE key='work_mode'"`
-	if [ mode -eq "normal" ]; then
+	workmode=`sqlite3 /usr/zeus/ontom.db "SELECT current_value FROM settings WHERE key='work_mode'"`
+	if [ workmode -eq "normal" ]; then
+		echo "启动到主页面"
 		browser -qws "http://127.0.0.1:8080/" &
-	elif [ mode -eq "correct" ]; then
+	elif [ workmode -eq "correct" ]; then
+		echo "启动到校准页面"
 		browser -qws "http://127.0.0.1:8080/jiaozhun.html" &
-	elif [ mode -eq "install" ]; then
+	elif [ workmode -eq "install" ]; then
+		echo "启动到安装配置页面"
 		browser -qws "http://127.0.0.1:8080/settings.html" &
 	else
+		echo "启动到默认页面"
 		browser -qws "http://127.0.0.1:8080/" &
 	fi
 	/usr/zeus/zeus
