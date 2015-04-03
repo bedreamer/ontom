66f0cd9f9b4fd739db71df662be80dcc60f54d08
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-03 06:26:08 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2495 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -1133,6 +1133,7 @@ int uart4_charger_date_evt_handle(struct bp_uart *self, struct bp_user *me, BP_U
     return ret;
 }
 
+
 int simple_box_read_evt_handle(struct bp_uart *self, struct bp_user *me, BP_UART_EVENT evt,
                      struct bp_evt_param *param)
 {
