7db80567fbc4c077aa9f5d441badbd7be836c6fa
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-10 01:10:38 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2568 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/bms.c
+++ ontom/bms.c
@@ -5,7 +5,8 @@
  * 读卡器 - 串口通信过程
  */
 #include "stdafx.h"
-#include "Hachiko.h"
+#include "bms.h"
+#include "charge.h"
 
 // 数据包超时心跳包, 定时器自动复位, 一个单位时间一次
 void heart_beart_notify_proc(Hachiko_EVT evt, void* _private, const struct Hachiko_food *self)
