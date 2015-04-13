8a29ce5c7be625c3a381f3a3876e95d21e6fe845
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-13 02:14:15 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2646 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- plugins/demo/demo.c
+++ plugins/demo/demo.c
@@ -5,7 +5,7 @@ int nr = 0;
 
 int exso_demo_init(void *p)
 {
-    printf("******  Plugin  demo  initialized  ******");
+    printf("******  Plugin  demo  initialized  ******\n");
     return ERR_OK;
 }
 
@@ -13,12 +13,12 @@ int exso_demo_main_loop(void *p)
 {
     nr ++;
     if ( 0 == (nr % 10) )
-    printf("*****   Plugin demo say: Hello %d", nr);
+    printf("*****   Plugin demo say: Hello %d\n", nr);
     return ERR_OK;
 }
 
 int exso_demo_exit(void *p)
 {
-    printf("******  Plugin  demo  exited  ******");
+    printf("******  Plugin  demo  exited  ******\n");
     return ERR_OK;
 }
