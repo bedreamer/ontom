be5f74bc93605d2ed423787b7a5003e23dc00da3
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-16 01:33:50 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2714 85aebdb3-f732-af43-9dbd-bcef24ea9265

---
+++ bmsdriver/beijingqiche/canbms.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdarg.h>
+#include <malloc.h>
+#include <ifaddrs.h>
+#include <signal.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <arpa/inet.h>
+
+#include "../../thirdpart/sqlite/sqlite3.h"
+
+#include "../../ontom/error.h"
+#include "../../lib/lj/stdafx.h"
+#include "../../lib/bms/father.h"
+#include "../../ontom/charge.h"
+
+int driver_main_proc(struct charge_job *, BMS_EVENT_CAN,
+                        struct bms_event_struct *, struct bmsdriver *)
+{
+    return ERR_OK;
+}
