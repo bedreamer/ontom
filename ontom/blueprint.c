df37ac58a25fd1388c38d285ffa1893e28049cd2
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-21 07:03:34 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2878 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2845,7 +2845,7 @@ int Increase_module_write_evt_handle(struct bp_uart *self, struct bp_user *me, B
     // 串口发送数据请求
     case BP_EVT_TX_FRAME_REQUEST:
         buff[ nr ++ ] = (unsigned char)(unsigned int)(me->_private);
-#if 1
+#if 0
     #if 1
         buff[ nr ++ ] = 0x06;
         buff[ nr ++ ] = 0x00;
