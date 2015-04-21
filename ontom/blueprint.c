607f729e7f5b3168b9a7a8f1fb0690f3cfbb228c
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-21 06:24:26 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2866 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -2789,12 +2789,12 @@ int Increase_convert_box_read_evt_handle(struct bp_uart *self, struct bp_user *m
             self->master->died ++;
         } else {
             //self->master->died ++;
-            if ( ! bit_read(task, S_CHARGE_M_1_ERR + (unsigned int)(me->_private)) ) {
+            if ( ! bit_read(task, S_CHARGE_M_1_ERR + (unsigned int)(me->_private) -1) ) {
             }
             log_printf(ERR, "UART: "RED("%d#模块通信中断, 请排查故障,(%d)"),
                        me->_private,
                        self->master->died);
-            bit_set(task, S_CHARGE_M_1_ERR + (unsigned int)(me->_private) );
+            bit_set(task, S_CHARGE_M_1_ERR + (unsigned int)(me->_private) -1 );
         }
         log_printf(WRN, "UART: %s:%d get signal TIMEOUT", __FUNCTION__, evt);
         break;
