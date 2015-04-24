01c87564534a22b3dbbce8eca2cde6e9c1936011
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-24 09:02:19 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3019 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bmsdriver/beijingqiche/canbms.c
+++ bmsdriver/beijingqiche/canbms.c
@@ -764,7 +764,7 @@ int about_packet_reciev_done(struct charge_job *thiz, struct bms_event_struct *p
             log_printf(WRN, "BMS: spn3073 range -400-0A gave: %d A",
                        thiz->bms.bms_charge_need_now.spn3073_need_current);
         } else {
-            thiz->need_I = 500;
+            thiz->need_I = 1000;
             //thiz->need_I = (thiz->bms.bms_charge_need_now.spn3073_need_current - 4000 )/-10.0f;
         }
 
