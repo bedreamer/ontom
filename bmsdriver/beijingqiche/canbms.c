344078eef3013933c8c7c72f1498c4a28694b815
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-24 03:25:35 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@2982 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- bmsdriver/beijingqiche/canbms.c
+++ bmsdriver/beijingqiche/canbms.c
@@ -577,6 +577,8 @@ int about_packet_reciev_done(struct charge_job *thiz, struct bms_event_struct *p
         bit_set(thiz, F_PCK_TX_CST);
         log_printf(INF, "BMS: PGN_CST 已经发送.");
         thiz->bms.charge_stage = CHARGE_STAGE_DONE;
+        log_printf(INF,
+          "BMS: CHARGER change stage to "RED("CHARGE_STAGE_DONE"));
         break;
     case PGN_CSD :// 0x001D00,
         bit_set(thiz, F_PCK_TX_CSD);
