72b47cdefca9f4c40b41fb65e6d501f74a76eac7
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-29 07:02:22 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3050 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/charge.c
+++ ontom/charge.c
@@ -832,13 +832,13 @@ void job_running(struct charge_task *tsk, struct charge_job *job)
     case JOB_WORKING:
         bit_set(tsk, F_CHARGE_LED);
 
-        if ( job->charge_mode == CHARGE_AUTO ) {
+        //if ( job->charge_mode == CHARGE_AUTO ) {
             sprintf(buff, "%d", (unsigned int)(job->need_V * 10.0f) );
             config_write("需求电压", buff);
             config_write("初始电压", buff);
             sprintf(buff, "%d", (unsigned int)(job->need_I * 10.0f) );
             config_write("需求电流", buff);
-        }
+        //}
 
         if ( ! bit_read(tsk, F_SYSTEM_CHARGE_ALLOW) ) {
             bit_clr(tsk, CMD_DC_OUTPUT_SWITCH_ON);
