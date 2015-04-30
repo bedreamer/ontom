8d8d86f9e0beaecf2953fb6abbeedaa89dc5d1db
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-04-30 06:14:36 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3064 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,16 +4,21 @@ echo "step into $WORKDIR"
 cd $WORKDIR
 
 echo "install system files"
-cp -p lib*.so install/usr/zeus/
-cp -p exso*.so install/usr/zeus/plugins/
-cp -p bmsdrv_*.so install/usr/zeus/drivers/
-cp -p `readlink me` install/usr/zeus/zeus
-cp -p ontom.db install/usr/zeus/
-cp -p ontom.cfg install/usr/zeus/
-cp -p user.cfg install/usr/zeus/
+mkdir -p install/usr/zeus
+mkdir -p install/usr/zeus/plugins
+mkdir -p install/usr/zeus/drivers
+mkdir -p install/srv/www
+
+cp lib*.so install/usr/zeus/
+cp exso*.so install/usr/zeus/plugins/
+cp bmsdrv_*.so install/usr/zeus/drivers/
+cp `readlink me` install/usr/zeus/zeus
+cp ontom.db install/usr/zeus/
+cp ontom.cfg install/usr/zeus/
+cp user.cfg install/usr/zeus/
 
 echo "install UI files"
-cp -pr UI_html/* install/srv/www/
+cp -r UI_html/* install/srv/www/
 
 echo "done"
 
