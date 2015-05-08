e1f79725dec1597e22def5171ac58a4af46a969c
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-05-08 05:38:56 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3131 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -5005,10 +5005,6 @@ continue_to_send:
                                tts, thiz->master->swap_time_modify,
                                thiz->rx_param.need_bytes);
                 } while (0);
-                errno = 0;
-                if ( 0 != fsync(thiz->dev_handle) ) {
-                    log_printf(WRN, "UART.driver: 写数据错误: %s", strerror(errno));
-                }
 
                 tcflush(thiz->dev_handle, TCIOFLUSH);
                 thiz->bp_evt_handle(thiz, BP_EVT_SWITCH_2_RX, NULL);
