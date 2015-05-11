3d60bb4696d7f613a793902c95fa4153cc5a4033
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-05-11 08:47:33 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3218 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -4798,8 +4798,9 @@ void *thread_uart_service(void *arg) ___THREAD_ENTRY___
                 thiz->tx_param.cursor = 0;
                 continue;
             }
-            tcdrain(thiz->dev_handle);
-            tcflush(thiz->dev_handle, TCIOFLUSH);
+            syncfs(thiz->dev_handle)
+            //tcdrain(thiz->dev_handle);
+            //tcflush(thiz->dev_handle, TCIOFLUSH);
             write(thiz->dev_handle, &thiz->tx_param.buff.tx_buff[thiz->tx_param.payload_size-1], 1);
             log_printf(INF, "after tcdrain: %d:%x", (int)(thiz->tx_param.payload_size *__usperbyte(thiz)),
                        thiz->tx_param.buff.tx_buff[thiz->tx_param.payload_size-1]);
