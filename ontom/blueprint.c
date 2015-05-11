28ceadc21e5e041559d95a170f8137ba4b04a4d8
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-05-11 04:34:48 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3161 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- ontom/blueprint.c
+++ ontom/blueprint.c
@@ -4991,6 +4991,7 @@ continue_to_send:
                     if ( thiz->role == BP_UART_MASTER ) {
                         // 主动设备，需要进行接收超时判定
                         thiz->rx_seed.ttl = thiz->master->ttw;
+                        thiz->rx_seed.remain = thiz->master->ttw;
                         log_printf(DBG_LV2, "UART: set rx timeout: %d", thiz->master->ttw);
                         Hachiko_resume(&thiz->rx_seed);
                     }
