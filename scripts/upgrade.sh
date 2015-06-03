50dd9b9bc88646d7e4b3ee42fea270bbabef6209
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-06-03 02:55:13 +0000

    git-svn-id: svn://192.168.1.8/ontom/trunk@3473 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -6,12 +6,12 @@ if [ $# -eq 1 ]; then
 	if [ -e "/tmp/zeus.version" ];then
 		rm /tmp/zeus.version
 	fi
-	wget "http://$1/www/VERSION" -O /tmp/zeus.version
+	wget "http://$1/www/VERSION" -O /tmp/zeus.version >/dev/null 2>&1
 	if [ -e "/tmp/zeus.version" ];then
 		if [ -e "/tmp/zeus.update.tar" ];then
 			rm /tmp/zeus.update.tar
 		fi
-		wget "http://$1/www/"`cat /tmp/zeus.version`"/zeus.update.tar" -O /tmp/zeus.update.tar
+		wget "http://$1/www/"`cat /tmp/zeus.version`"/zeus.update.tar" -O /tmp/zeus.update.tar 2>&1
 		if [ -e "/tmp/zeus.version" ];then
 			tar -C / -xvf /tmp/zeus.update.tar > /usr/zeus/update.log
 			echo "Upgrade success version "`cat /tmp/zeus.version`
