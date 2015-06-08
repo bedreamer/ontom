a98d7233964b9d6c439f457ff5fdf6a3eb9e3d90
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-06-08 06:44:01 +0000

    修改启动程序脚本
    git-svn-id: svn://192.168.1.8/ontom/trunk@3540 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -5,6 +5,22 @@ echo "执行数据库预配置..."
 /usr/zeus/preconfig.sh /usr/zeus/ontom.db
 echo "开始启动主程序..."
 
+if [ ! -d /tmp/hellhound ] && [ ! -e /tmp/hellhound ];then 
+    mkdir /tmp/hellhound
+fi
+
+if [ ! -e /tmp/hellhound/zeus_pid ];then
+	echo $$ > /tmp/hellhound/zeus_pid
+else
+    if [ ! -e "/proc/`cat "/tmp/hellhound/"$MD5"_pid"`" ];then
+        echo "无效zeus进程, 自动清理."
+        rm "/tmp/hellhound/zeus_pid"
+    else
+        echo "系统程序正常, PID=`cat "/tmp/hellhound/zeus_pid"`"
+        exit 1;
+    fi
+fi
+
 while [ true ]; do
 	for PID in `pidof browser` `pidof zeus`;do
 		kill -9 $PID
@@ -12,22 +28,27 @@ while [ true ]; do
 	workmode=`sqlite3 /usr/zeus/ontom.db "SELECT current_value FROM settings WHERE key='work_mode'"`
 	if [ $workmode == "normal" ]; then
 		echo "启动到主页面"
-		browser -qws "http://127.0.0.1:8080/" &
+		hellhound.sh browser -qws "http://127.0.0.1:8080/" &
+		BROPID=$!
 	elif [ $workmode == "correct" ]; then
 		echo "启动到校准页面"
-		browser -qws "http://127.0.0.1:8080/jiaozhun.html" &
+		hellhound.sh browser -qws "http://127.0.0.1:8080/jiaozhun.html" &
+		BROPID=$!
 	elif [ $workmode == "install" ]; then
 		echo "启动到安装配置页面"
-		browser -qws "http://127.0.0.1:8080/settings.html" &
+		hellhound.sh browser -qws "http://127.0.0.1:8080/settings.html" &
+		BROPID=$!
 	else
 		echo "启动到默认页面"
-		browser -qws "http://127.0.0.1:8080/" &
+		hellhound.sh browser -qws "http://127.0.0.1:8080/" &
+		BROPID=$!
 	fi
 	/usr/zeus/zeus >> /tmp/zeus.log
 	echo "检测到程序主动退出, 1秒后自动重启..."
+	kill -9 $BROPID
 	sleep 1;
+	echo "关闭地狱犬.."
 	echo "重新预配置数据库"
 	/usr/zeus/preconfig.sh /usr/zeus/ontom.db
 	echo "正在重启...."
-
 done
