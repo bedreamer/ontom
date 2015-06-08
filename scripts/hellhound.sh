c2f499831c243f7b1a4999e93ed713db5ae047e2
tom <tom@85aebdb3-f732-af43-9dbd-bcef24ea9265>
2015-06-08 06:24:33 +0000

    修改地狱犬程序，实现软件看门狗.
    git-svn-id: svn://192.168.1.8/ontom/trunk@3539 85aebdb3-f732-af43-9dbd-bcef24ea9265

--- scripts/hellhound.sh
+++ scripts/hellhound.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
 
-echo "地狱犬已启动，启动程序 $*"
+if [ ! -d /tmp/hellhound ] && [ ! -e /tmp/hellhound ];then 
+	mkdir /tmp/hellhound
+fi
 
-while true
-	do
-		ps | grep 'S \+zeus' > /dev/null
-		if [ $? == 0 ]; then
-			sleep 1;
+RP=`which $1`
+MD5=`md5sum $RP|awk '{print $1}'`
+
+if [ -e "/tmp/hellhound/"$MD5"_pid" ];then
+	if [ ! -e "/proc/`cat "/tmp/hellhound/"$MD5"_pid"`" ];then
+		echo "地狱犬嗅探到无效进程, 自动清理."
+		rm "/tmp/hellhound/"$MD5"_pid"
+	else
+		echo "程序正常, PID=`cat "/tmp/hellhound/"$MD5"_pid"`"
+		exit 1;
+	fi
+fi
+
+if [ -e "`which $1`" ] && [ -x "`which $1`" ];then
+	while [ true ];do
+		if [ ! -e "/tmp/hellhound/"$MD5"_pid" ];then
+			$* &
+			PID=$!
+			echo $PID > "/tmp/hellhound/"$MD5"_pid"
+			echo "地狱犬已启动，看守进程 <PID=$PID;$*>"
 		else
-			/usr/zeus/zeus
-			echo "主程序已退出..."
+			if [ ! -e "/proc/$PID" ];then
+				echo "地狱犬嗅探到<$1>退出，1秒后重启..."
+				rm "/tmp/hellhound/"$MD5"_pid"
+			else
+				sleep 1
+				continue
+			fi
 		fi
+		sleep 1
 	done
+else
+	exit 1;
+fi
+
+exit 1
